0c25c738e5e1932dd5a6d7757c070728d6bd3103
Peter Shershov <peters@wix.com>
2022-02-08 13:57:47 +0200

    change demo to board

--- _wcs/boards/App/App.demo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createDemo } from '@wixc3/react-simulation';
-import App from '../../../src/App';
-
-export default createDemo({
-    name: 'App',
-    demo: () => <App />,
-});
