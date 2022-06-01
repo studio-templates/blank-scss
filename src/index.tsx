76960455f0a572623398796475e15fdf5a633719
Yarden Porat <yardenporat@gmail.com>
2022-06-01 16:10:34 +0300

    use only

--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// /Users/adenpry / Projects / studio - templates / blank - scss / src / index.sc;
+// /Users/adenpry / Projects / studio -
+//     templates / blank -
+//     scss / src / index.scss;
+//    Cannot find compiled file
+//Users/yardenp/Projects/studio-templates/blank-scss/src/index.scss
+//Users/yardenp/Projects/studio-templates/blank-scss/src/index.scss
