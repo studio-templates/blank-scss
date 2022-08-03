8aeb48cdd93c7928bd977d809f350c246ae71549
Sagiv Dayan <sagiv.keeper@gmail.com>
2022-08-03 19:11:36 +0300

    fix: change createRoot container

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-createRoot(document.body.appendChild(document.createElement("div"))).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
